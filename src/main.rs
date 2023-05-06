af746c98d8d5427756c1763b3ebcf51f264daf05
Ichsan Rahardianto <irahardianto@Ichsans-MacBook-Pro.local>
2023-05-06 20:52:04 +0700

    implement logger using env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+use env_logger::Env;
+
 use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let connection = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,20 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::dev::Server;
+use actix_web::web::Data;
+use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 
 use crate::routes::{health_check, subscriptions};
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the connection in a smart pointer so that it can be shared across threads
-    let connection = web::Data::new(connection);
+    let connection = Data::new(connection);
     // Capture `connection` from the surrounding environment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check::health_check))
             .route("/subscriptions", web::post().to(subscriptions::subscribe))
             .app_data(connection.clone())
