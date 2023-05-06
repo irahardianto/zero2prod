4712e5fd4158f1ca8c6ddab9c347b56573d9fa18
Ichsan Rahardianto <irahardianto@Ichsans-MacBook-Pro.local>
2023-05-06 21:25:49 +0700

    implement log in subscriptions with requestid

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,9 @@ pub struct FormData {
 
 // Let's start simple: we always return a 200 OK
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!("request_id {} - Adding '{}' '{}' as a new subscriber.", request_id, form.email, form.name);
+    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +29,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("request_id {} - New subscriber details have been saved", request_id);
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            print!("Failed to execute query: {}", e);
+            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
